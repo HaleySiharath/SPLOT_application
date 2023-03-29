ca43ec8331613fc7a9b21c29e87cb73decedc80c
unknown <haley.siharat@gmail.com>
2023-03-29 14:52:23 -0400

    test

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask_bcrypt import Bcrypt
 import spot
 import sqlalchemy
 
+# just adding something for a test
 
 app = Flask(__name__) #static_url_path='/static' (??) (ignore)
 bcrypt = Bcrypt(app)
